2636884295a74a725acee668b127127886c8586b
SanchiChopra <choprasanchi2@gmail.com>
2019-06-19 22:22:57 +0530

    connections made!

--- login_example.py
+++ login_example.py
@@ -29,7 +29,7 @@ def login():
     login_user = users.find_one({'name' : request.form['username']})
     print(login_user)
     if login_user:
-        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+        if bcrypt.hashpw(request.form['pass'].encode('utf-8'), login_user['password']) == login_user['password']:
             session['username'] = request.form['username']
             return "confirm login"
 
