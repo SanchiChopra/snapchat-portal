5f27885370dbadc5382ee6d9f445268caa828df8
Sanchi Chopra <35348387+SanchiChopra@users.noreply.github.com>
2019-07-01 14:45:03 +0530

    removed unnecessary code

--- app/README.md
+++
@@ -1,3 +0,0 @@
-# snapchat-portal
-
-A simple API for login by an existing user and registration of a new user...
--- app/env.py
+++
@@ -1,2 +0,0 @@
-db_link = 'mongodb+srv://test:test@cluster0-mlywv.mongodb.net/test?retryWrites=true&w=majority'
-secret = 'snapchatproject'
